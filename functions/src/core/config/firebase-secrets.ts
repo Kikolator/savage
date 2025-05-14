9c81859b061ea0ea0af0e6c0ece87fb870ccfbb9
Nicholas Pijpers <61523024+Kikolator@users.noreply.github.com>
2025-05-14 16:52:06 +0200

    add office rnd secret

--- functions/src/core/config/firebase-secrets.ts
+++ functions/src/core/config/firebase-secrets.ts
@@ -3,9 +3,11 @@ import { defineSecret } from 'firebase-functions/params';
 interface FirebaseSecrets {
   sendgridApiKey: ReturnType<typeof defineSecret>;
   typeformSecretKey: ReturnType<typeof defineSecret>;
+  officeRndSecretKey: ReturnType<typeof defineSecret>;
 }
 
 export const firebaseSecrets: FirebaseSecrets = {
   sendgridApiKey: defineSecret('SENDGRID_API_KEY'),
   typeformSecretKey: defineSecret('TYPEFORM_SECRET'),
+  officeRndSecretKey: defineSecret('OFFICE_RND_SECRET'),
 };
---
+++ functions/src/core/config/office-rnd-config.ts
@@ -0,0 +1,5 @@
+export const officeRndConfig = {
+  clientId: '1234567890',
+  grantType: 'client_credentials',
+  scopes: 'officernd.api.read officernd.api.write flex.community.members.read flex.community.members.create flex.community.members.update flex.community.opportunities.read flex.community.opportunities.create flex.community.opportunities.update flex.community.checkins.read flex.community.checkins.create flex.community.checkins.update flex.space.resources.read flex.space.bookings.read flex.space.bookings.create flex.space.bookings.update',
+};
