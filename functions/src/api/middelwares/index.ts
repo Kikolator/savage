704146f18bac1ec52d333385babdd5c67a6b1b3d
Nicholas Pijpers <61523024+Kikolator@users.noreply.github.com>
2025-05-23 11:03:51 +0200

    Add raw body option for typeform webhooks

--- functions/src/api/middelwares/index.ts
+++ functions/src/api/middelwares/index.ts
@@ -2,10 +2,31 @@ import { Request, Response, NextFunction } from 'express';
 import * as bodyParser from 'body-parser';
 import { setTypeformSignature } from './typeform/typeform-signature';
 
+// custom verifier function, only to requests from Typeform
+const rawBodySaver = (
+  req: Request,
+  res: Response,
+  buf: Buffer,
+  encoding: BufferEncoding
+) => {
+  if (
+    req.headers['user-agent'] === 'Typeform Webhooks' &&
+    req.headers['typeform-signature'] &&
+    buf &&
+    buf.length
+  ) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+};
+
+const options = {
+  verify: rawBodySaver,
+};
+
 export const interceptors: Array<
   (req: Request, res: Response, next: NextFunction) => void
 > = [
   bodyParser.urlencoded({ extended: false }),
-  bodyParser.json(),
+  bodyParser.json(options),
   setTypeformSignature,
 ];
