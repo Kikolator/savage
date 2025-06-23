7d475e726256fc49664dd32d5cba5a945ba37f59
Nic Pijpers <nicholas@kikolator.dev>
2025-06-23 14:42:07 +0200

    init flutter project with stacked

---
+++ frontend/referral/lib/ui/views/home/home_viewmodel.dart
@@ -0,0 +1,36 @@
+import 'package:referral/app/app.bottomsheets.dart';
+import 'package:referral/app/app.dialogs.dart';
+import 'package:referral/app/app.locator.dart';
+import 'package:referral/ui/common/app_strings.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class HomeViewModel extends BaseViewModel {
+  final _dialogService = locator<DialogService>();
+  final _bottomSheetService = locator<BottomSheetService>();
+
+  String get counterLabel => 'Counter is: $_counter';
+
+  int _counter = 0;
+
+  void incrementCounter() {
+    _counter++;
+    rebuildUi();
+  }
+
+  void showDialog() {
+    _dialogService.showCustomDialog(
+      variant: DialogType.infoAlert,
+      title: 'Stacked Rocks!',
+      description: 'Give stacked $_counter stars on Github',
+    );
+  }
+
+  void showBottomSheet() {
+    _bottomSheetService.showCustomSheet(
+      variant: BottomSheetType.notice,
+      title: ksHomeBottomSheetTitle,
+      description: ksHomeBottomSheetDescription,
+    );
+  }
+}
